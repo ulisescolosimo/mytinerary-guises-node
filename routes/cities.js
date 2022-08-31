e6816a24f075d83720e553c6a5259ef229304b90
ulisescolosimo <colosimo101@gmail.com>
2022-08-31 11:27:01 -0300

    Agregamos controlador de update

--- routes/cities.js
+++ routes/cities.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {create, read, readAll, deleted} = require('../controllers/cityController')
+const {create, read, readAll, deleted, update} = require('../controllers/cityController')
 
 router.post('/', create);
 router.get('/', readAll);
 router.get('/:id', read);
 router.delete('/:id', deleted);
+router.patch('/:id', update);
 
 
 module.exports = router;
