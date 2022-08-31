515de4ea12d6fcca17301e21d4d6f51a300e6744
ulisescolosimo <colosimo101@gmail.com>
2022-08-31 17:22:42 -0300

    Agregamos ruta para buscar una ciudad por query

--- routes/cities.js
+++ routes/cities.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const {create, read, readAll, deleted, update} = require('../controllers/cityController')
+const {create, read, readAll, deleted, update, readCity} = require('../controllers/cityController')
 
+router.get('/all/', readAll);
+router.get('/', readCity)
 router.post('/', create);
-router.get('/', readAll);
 router.get('/:id', read);
 router.delete('/:id', deleted);
-router.patch('/:id', update);
+router.put('/:id', update);
 
 
 module.exports = router;
