10d5571d06dd5a0c1dfd7a95cf118ff3e710018f
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-02 14:08:36 -0300

    creacion de modelo user

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+      name: {type: String, required: true},
+      lastName:{type: String, required: true},
+      mail:{type: String, required: true},
+      password:{type: String, required: true},
+      photo:{type: String, required: true},
+      country:{type: String, required: true},
+
+})
+
+
+const User = mongoose.model(
+      'Users',
+      UserSchema
+)     
+ 
+module.exports = User
