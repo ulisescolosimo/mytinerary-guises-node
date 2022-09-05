f455658a13234f7b95567df53fe5b9a8ed5beb8a
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-05 18:32:25 -0300

    agregado de ruta de itinerary

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var itineraryRouter = require('./routes/itineraries');
+
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/itineraries', itineraryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
