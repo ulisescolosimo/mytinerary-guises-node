8bdbaad344c2ed5f9682ea355ae941cc47694af7
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-05 18:33:18 -0300

    cambio de titulo de enturador principal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const citiesRouter = require('./cities')
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Amazing Events' });
+  res.render('index', { title: 'My Tinerary - Guises' });
 });
 
 router.use('/cities', citiesRouter)
