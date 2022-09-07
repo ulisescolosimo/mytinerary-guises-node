4c84e2150d4ff8c0bb45063a427ebab4198c466d
ulisescolosimo <colosimo101@gmail.com>
2022-09-07 00:34:48 -0300

    Modificamos rutas

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const citiesRouter = require('./cities')
 const usersRouter = require('./usersLogin')
 const itinerariesRouter = require('./itineraries.js')
 const commentsRouter = require('./comments.js')
+const activitiesRouter = require('./activities.js')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Amazing Events' });
@@ -14,5 +15,6 @@ router.use('/cities', citiesRouter)
 router.use('/auth', usersRouter)
 router.use('/itineraries', itinerariesRouter)
 router.use('/comments', commentsRouter)
+router.use('/activities', activitiesRouter)
 
 module.exports = router;
