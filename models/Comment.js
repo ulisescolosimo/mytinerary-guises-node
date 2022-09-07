e60f45b043fabc7ab466989c16391ce9fd91f0dd
ulisescolosimo <colosimo101@gmail.com>
2022-09-07 00:33:43 -0300

    Modificamos modelo de comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 const CommentSchema = new mongoose.Schema({
       comment: {type: String, required: true},
-      user: {type: String, required: true},
-      itinerary: {type: String, required: true},
+      user: {type: mongoose.Types.ObjectId, ref:'users'},
+      itinerary: {type: mongoose.Types.ObjectId, ref:'itineraries'},
 })
 
 const Comment = mongoose.model(
-      'Comments',
+      'comments',
       CommentSchema
 )     
 
