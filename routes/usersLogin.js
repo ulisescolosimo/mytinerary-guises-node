1ac4f5bd364cd3bf8742936882c975275fa7031c
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-12 11:40:01 -0300

    nueva ruta get para verify mail

--- routes/usersLogin.js
+++ routes/usersLogin.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {signUp, getUsers, deleteUser} = require('../controllers/userController')
+const {signUp, getUsers, deleteUser, verifyMail} = require('../controllers/userController')
 
 router.post('/signup', signUp);
 router.get('/', getUsers);
 router.delete('/:id', deleteUser)
+router.get('/verify/:code', verifyMail)
 
 module.exports = router;
