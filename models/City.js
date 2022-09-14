463604d39e092bf777258f7dd56a7c8ba6f622ad
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-14 00:21:49 -0300

    agregado de propiedades a modelo city

--- models/City.js
+++ models/City.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 
 const CitySchema = new mongoose.Schema({
-      city: {type: String, required: true},
-      country: {type: String, required: true},
-      photo: {type: String, required: true},
+      city: {type: String, required: true,min:3},
+      country: {type: String, required: true, min:3},
+      photo: {type: String, required: true, validate: function (value){
+             if (!value.endsWith('jpg') || !value.endsWith('png') && !value.startsWith('http')) {
+               throw new Error('The url must start with "http"')   
+            }}},
       population: {type: Number, required: true, min: 1000,
       max: 100000000},
       foundation: {type: Date, required: true},
