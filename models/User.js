dd2bda7e635650cfd029fb6b4a2c49f6698abc50
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-14 00:22:16 -0300

    agregado de propiedades a modelo user

--- models/User.js
+++ models/User.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-      name: {type: String, required: true},
-      email:{type: String, required: true},
-      pass:[{type: String, required: true}],
+      name: {type: String, required: true, min: 3, max: 35},
+      email:{type: String, required: true,validate: function (value){
+            if (! value.endsWith('gmail.com') || ! value.endsWith('hotmail.com')) {
+               throw new Error('INVALID_EMAIL')   
+            }
+      } },
+      pass:[{type: String, required: true, min:8, max:35}],
       role: {type: String, required: true},
-      photo:{type: String, required: true},
+      photo:{type: String, required: true, validate: function (value){
+            if (! value.startsWith('http')) {
+               throw new Error('INVALID_URL')   
+            }
+      }},
       country:{type: String, required: true},
       from: [{type: String, required: true}],
       logged: {type: Boolean, required: true},
