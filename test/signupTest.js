3ba642e58e8a5d42d02cc47bfd6c6f4c5248ec6d
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-15 23:22:34 -0300

    correccion de tests existentes y creacion de nuevos

--- test/signupTest.js
+++ test/signupTest.js
@@ -1,35 +1,52 @@
-/* const request = require('supertest')  
+const request = require('supertest')  
 const app = require('../app')         
 const {assert} = require('chai')
 
 
 describe('POST /auth/signup', function () {
 
-      it('must respond with 201 status code', function (done) {
+      it('must respond with 200 status code', function (done) {
             request(app)
                 .post('/auth/signup')
                 .send({
-                  name: "Guido Seia" ,
-                  email: "guidoseia5@gmail.com",
-                  pass: ["elcordobee"],
+                  name: "Guidos Seia" ,
+                  email: "guidoseia3354@gmail.com",
+                  pass: "elcordobee",
                   role: "user",
                   photo: "https://pbs.twimg.com/profile_images/973255823148355584/PQrQ_HzA_400x400.jpg",
                   country: "Argentina",                  
-                  from: ["form"]               
+                  from: "form"               
                 })
-                .expect(201, done)
+                .expect(200, done)
         }),
 
       it('Must respond with 400 status code', function (done) {
             request(app)
                 .post('/auth/signup')
-                .send({  
+                .send({ 
+                  name: "Guido Seia" ,
+                  email: "guidoseia23",
+                  pass: "elcordobee",
+                  role: "user",
+                  photo: "https://pbs.twimg.com/profile_images/973255823148355584/PQrQ_HzA_400x400.jpg",
+                  country: "Argentina",                  
+                  from: "form"     
                 })
-                .expect(400)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    return done();
-                })
+                .expect(400, done)               
         })
 
-})  */
+        it('Must respond with 201 status code', function (done) {
+          request(app)
+              .post('/auth/signup')
+              .send({ 
+                name: "Ulises colosimo" ,
+                email: "ulisescolosimo@gmail.com",
+                pass: "ulisesAcadee",
+                role: "user",
+                photo: "https://pbs.twimg.com/profile_images/973255823148355584/PQrQ_HzA_400x400.jpg",
+                country: "Argentina",                  
+                from: "google"     
+              })
+              .expect(201, done)               
+      })
+})  
