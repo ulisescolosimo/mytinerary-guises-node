a4574eef038ffa3de50603c983cca0270d5938e4
ulisescolosimo <colosimo101@gmail.com>
2022-09-16 00:17:06 -0300

    Modificamos validaciones modelo city

--- models/City.js
+++ models/City.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose')
 const CitySchema = new mongoose.Schema({
       city: {type: String, required: true,min:3},
       country: {type: String, required: true, min:3},
-      photo: {type: String, required: true, validate: function (value){
-             if (!value.endsWith('jpg') || !value.endsWith('png') && !value.startsWith('http')) {
-               throw new Error('The url must start with "http"')   
-            }}},
+      photo: {type: String, required: true},
       population: {type: Number, required: true, min: 1000,
       max: 100000000},
       foundation: {type: Date, required: true},
