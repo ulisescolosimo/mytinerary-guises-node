1cb651156c9c943d76f91f21fd6c32f0bdcada4c
ulisescolosimo <colosimo101@gmail.com>
2022-09-20 13:01:13 -0300

    Creamos ruta para verify login

--- routes/usersLogin.js
+++ routes/usersLogin.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
-const {signUp, getUsers, deleteUser, verifyMail, signIn, signOut} = require('../controllers/userController')
+const {signUp, getUsers, deleteUser, verifyMail, signIn, signOut, verifyToken} = require('../controllers/userController')
 
 router.patch('/:id', signOut)
 router.post('/signup', signUp);
@@ -9,5 +9,6 @@ router.post('/signin', signIn)
 router.get('/', getUsers);
 router.get('/verify/:code', verifyMail)
 router.delete('/:id', deleteUser)
+
 
 module.exports = router;
