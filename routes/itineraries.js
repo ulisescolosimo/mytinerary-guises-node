70a385506925b1d8c13e117423db9c949654a2bf
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-20 15:26:34 -0300

    agregado de requerimientos passport

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
-
+const passport = require("../config/passport");
 
 const {createIti, deleteIti, updateIti, readIti, readAll, likeDislike} = require('../controllers/itineraryController')
 
+router.get('/', readIti)
+router.get('/all', readAll)
 router.post('/', createIti);
 router.delete('/:id', deleteIti);
 router.patch('/:id', updateIti);
-router.get('/', readIti)
-router.get('/all', readAll)
-router.patch('/likes', passport.authenticate('jwt', {session:false}), likeDislike);
+router.patch('/like/:id', passport.authenticate('jwt', {session:false}), likeDislike)
 
 module.exports = router;
