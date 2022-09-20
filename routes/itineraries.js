41606b4c8fca320813a2342c450d53bba291830b
ulisescolosimo <colosimo101@gmail.com>
2022-09-20 13:00:40 -0300

    Creamos ruta para like dislike

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
-const {createIti, deleteIti, updateIti, readIti, readAll} = require('../controllers/itineraryController')
+
+const {createIti, deleteIti, updateIti, readIti, readAll, likeDislike} = require('../controllers/itineraryController')
 
 router.post('/', createIti);
 router.delete('/:id', deleteIti);
 router.patch('/:id', updateIti);
 router.get('/', readIti)
 router.get('/all', readAll)
+router.patch('/likes', passport.authenticate('jwt', {session:false}), likeDislike);
 
 module.exports = router;
