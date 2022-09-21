512f5dd581de9b63741c7b27ace2018290784cbe
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-09-21 10:53:58 -0300

    agregado de ruta post y patch de comentarios

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { readAllComments, createComment, deleted } = require('../controllers/commentController');
+const passport = require("../config/passport");
+
+const { readAllComments, createComment, deleted, editComment } = require('../controllers/commentController');
 
 router.get('/', readAllComments);
-router.post('/', createComment);
+router.post('/:id', passport.authenticate('jwt', {session:false}), createComment);
+/* router.patch('/:id', passport.authenticate('jwt', {session:false}), editComment); */
 router.delete('/:id', deleted)
 
 module.exports = router;
