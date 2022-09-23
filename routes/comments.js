8d6f4a7481c2fe08281aa1b509bd3d296008366c
ulisescolosimo <colosimo101@gmail.com>
2022-09-23 12:09:21 -0300

    Last fix route

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const passport = require("../config/passport");
 const { readAllComments, createComment, deleted, modifyComment } = require('../controllers/commentController');
 
 router.get('/', readAllComments);
-router.post('/:id', passport.authenticate('jwt', {session:false}), createComment);
+router.post('/', passport.authenticate('jwt', {session:false}), createComment);
 router.patch('/:id', passport.authenticate('jwt', {session:false}), modifyComment); 
 router.delete('/:id',passport.authenticate('jwt', {session:false}) ,deleted)
 
